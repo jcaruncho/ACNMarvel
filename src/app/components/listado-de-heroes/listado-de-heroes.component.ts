2dd88ae35a447712f814e79f03bf794e7c4ee196
Julio Caruncho <julio.caruncho.a@gmail.com>
2022-01-29 22:04:30 -0300

    fix[acnmarvel listado-de-heroes.component]: se realiza el unsubscribe al observable en el metodo ngOnDestroy

--- src/app/components/listado-de-heroes/listado-de-heroes.component.ts
+++ src/app/components/listado-de-heroes/listado-de-heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { HeroesService } from '../../services/heroes.service';
 import { Heroe } from '../../classes/heroe';
@@ -16,16 +16,20 @@ export class ListadoDeHeroesComponent implements OnInit {
   public searchString: string;
   public heroesList$: Observable<Array<Heroe>>;
   public heroesList: Array<Heroe>;
+  public heroesSubscription: Subscription;
 
   constructor(private heroesService: HeroesService, private store: Store<{ heroes: Array<Heroe> }>) { }
 
   ngOnInit(): void {
     this.heroesList$ = this.store.select('heroes');
-    this.heroesList$.subscribe(({ heroes }: any) => {
+    this.heroesSubscription = this.heroesList$.subscribe(({ heroes }: any) => {
       heroes ? this.heroesList = heroes.data : '';
-      console.log(this.heroesList);
     });
     this.heroesService.getHeroes();
+  }
+
+  ngOnDestroy(): void {
+    this.heroesSubscription.unsubscribe();
   }
 
   submitSearch() {
